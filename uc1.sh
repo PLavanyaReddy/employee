61e1db03fe79f74b33dce9733e38df111914bbc3
unknown <lavanyareddy19994@gmail.com>
2020-07-14 15:43:59 +0530

    welcome

--- uc1.sh
+++ uc1.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-is_present=$(( RANDOM%2 ))
-if [ $is_present -eq 1 ]
-then
-	echo "Employee is present"
-else
-	echo "Employee is absent"
-fi
+echo "Welcome to employee wage calculation program"
+
